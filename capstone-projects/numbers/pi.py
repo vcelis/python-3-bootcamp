6b1fdeca6a3c8fe9e56e39ea8ef186eaeb29deba
Vincent Celis <vincent.celis@gmail.com>
2018-09-05 13:26:19 -0500

    Capstone project
    Capstone project

---
+++ capstone-projects/numbers/pi.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Title       :   Find PI to the Nth Digit
+Subdomain   :   Numbers
+Author      :   Vincent Celis
+Created     :   20 July 2018
+
+Enter a number and have the program generate PI up to that many decimal
+places. Keep a limit to how far the program will go.
+"""
